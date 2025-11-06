f545fdc13508083f891b413debe3615d9ca539d8
Joaqukn <162461160+Joaqukn@users.noreply.github.com>
2025-11-06 13:52:30 -0300

    test loader

--- server.js
+++ server.js
@@ -295,6 +295,8 @@ app.get(/.*/, (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+
+
 // === INICIO SERVIDOR ===
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => console.log(`Servidor corriendo en http://localhost:${PORT}`));
